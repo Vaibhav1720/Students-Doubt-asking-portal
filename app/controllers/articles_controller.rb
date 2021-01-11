075730ad9850dc11133dea88b7bcfd05b35c5497
Vaibhav Singh <vaibhavsingh1720@gmail.com>
2021-01-11 12:33:58 +0530

    Bugs of Student page updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,29 +21,30 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  # def edit
+  #   @article = Article.find(params[:id])
+  # end
 
-  def update
-    @article = Article.find(params[:id])
+  # def update
+  #   @article = Article.find(params[:id])
 
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render :edit
-    end
-  end
+  #   if @article.update(article_params)
+  #     redirect_to @article
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+  # def destroy
+  #   @article = Article.find(params[:id])
+  #   @article.destroy
 
-    redirect_to root_path
-  end
+  #   redirect_to root_path
+  # end
 
   private
     def article_params
       params.require(:article).permit(:title, :body)
     end
 end
+ 
